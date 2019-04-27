f70d4ac8b49d8afa91ac39dc78cf8a0d9364f621
Fufuhu <e238058r@hotmail.co.jp>
2019-04-27 14:45:30 +0900

    サンプルを追加

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import picamera
+from datetime import datetime
+cam = picamera.PiCamera()
+cam.vflip = True
+file_name = datetime.now().strftime(“%Y%m%d%H%M%S”)
+cam.capture('./' + file_name + '.jpg' )
+cam.close()
