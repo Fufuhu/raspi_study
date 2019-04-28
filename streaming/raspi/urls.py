f4b9e9578beef2deb988388e9500c0c355a1d4e6
Fufuhu <e238058r@hotmail.co.jp>
2019-04-28 15:52:24 +0900

    ロゴを載せるところまで出来た

--- streaming/raspi/urls.py
+++ streaming/raspi/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from raspi.views.base_view import BaseView
 from raspi.views.stream_view import StreamView
 from raspi.views.filtered_stream_view import FilteredStreamView
+from raspi.views.logo_stream_view import LogoStreamView
 
 urlpatterns = [
     path('filtered', FilteredStreamView.as_view(), name="filtered_stream"),
     path('stream', StreamView.as_view(), name='stream'),
+    path('logo', LogoStreamView.as_view(), name='logo_stream'),
     path('', BaseView.as_view(), name="base"),
 ]
---
+++ streaming/raspi/views/logo_stream_view.py
@@ -0,0 +1,42 @@
+from django.views.generic import View
+from django.shortcuts import render, redirect
+from django.http import StreamingHttpResponse
+import cv2
+
+from raspi.services.video_camera import VideoCamera
+
+cam =  VideoCamera()
+
+def overlay_logo(frame):
+    img = cv2.imread("/home/fujiwara/repositories/raspi_study/streaming/raspi/views/rancher.png")
+    # img = cv2.imread("rancher.png", -1)
+
+    rows, cols, _ = img.shape
+    roi = frame[0:rows, 0:cols]
+    img = img[0:rows, 0:cols]
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(img_gray, 10, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
+    
+    bg = cv2.bitwise_and(roi,roi,mask=mask_inv)
+    fg = cv2.bitwise_and(img,img,mask=mask)
+
+    dst = cv2.add(bg, fg)
+    frame[0:rows, 0:cols] = dst
+
+    # return frame
+    return frame
+
+
+class LogoStreamView(View):
+    def get_stream(self):
+        # https://stackoverflow.com/questions/49680152/opencv-live-stream-from-camera-in-django-webpage
+
+        while True:
+            frame = cam.get_filtered_frame(overlay_logo)
+            yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+    def get(self, request):
+        return StreamingHttpResponse(self.get_stream(), content_type='multipart/x-mixed-replace;boundary=frame')
